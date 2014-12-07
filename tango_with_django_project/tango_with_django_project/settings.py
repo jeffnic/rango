412358394e9a2e72d942ee94279515f663ce1ec0
jeff nichols <ffej.slohcin@gmail.com>
2014-12-06 19:45:49 -0600

    12-6

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -81,4 +81,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+from os.path import join
+TEMPLATE_DIRS = (
+    join(BASE_DIR, 'templates'),
+)
